049a3db29114f77a8e98f11ffe306775ebbf1a29
Amariee Cooper <90587117+AmarieeCooper@users.noreply.github.com>
2023-10-19 14:03:31 -0500

    AddedCode
    added things to the if statement for NavBar component.

--- ChangeManageSystemFrontEnd/src/Components/NavBar.jsx
+++ ChangeManageSystemFrontEnd/src/Components/NavBar.jsx
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 
-var navBarNumber = 1 
+var navBarNumber = 1
 
 class NavBar extends Component {
     render() {
-    if (navBarNumber == 1) {
+    if (navBarNumber === 1) {
     // NavBar for Entry
     return(
 
         <div>
-            <div id="u171" class="ax_default button">
-            <div id="u171_div" class=""></div>
-            <div id="u171_text" class="text ">
-                <p><span>Submit Request</span></p>
-                </div>
-            </div>
-
             <div id="u172" class="ax_default button">
             <div id="u172_div" class=""></div>
-            <a href="/ListScreenApp" id="u172_text" class="text ">
+            <a href="/Review" id="u172_text" class="text ">
               <p><span>View Change Reuqests</span></p>
+                </a>
+            </div>
+
+            <div id="u174" class="ax_default button">
+            <div id="u174_div" class=""></div>
+            <a href="/ArchivedChangesApp" id="u174_text" class="text ">
+              <p><span>View Change Request Archive</span></p>
                 </a>
             </div>
 
@@ -30,23 +30,87 @@ class NavBar extends Component {
                 </a>
             </div>
 
+            
+        </div>
+        //Stuff for Submit Request, View Change Requests, View Change Request Archive, Logout
+    )
+    } else if (navBarNumber === 2) {
+    // NavBar for Review
+    return (
+        <div>
+
             <div id="u174" class="ax_default button">
             <div id="u174_div" class=""></div>
             <a href="/ArchivedChangesApp" id="u174_text" class="text ">
               <p><span>View Change Request Archive</span></p>
                 </a>
             </div>
+
+            <div id="u173" class="ax_default button">
+                <div id="u173_div" class=""></div>
+                <a href="/Login" id="u173_text" class="text">
+                    <p><span>Log out</span></p>
+                </a>
+            </div>
+
+            <div id="u173" class="ax_default button">
+            <div id="u173_div" class=""></div>
+            <a href="/Login" id="u173_text" class="text ">
+              <p><span>Log out</span></p>
+                </a>
+            </div>
         </div>
-        //Stuff for Submit Request, View Change Requests, View Change Request Archive, Logout
-    )
-    } else if (navBarNumber == 2) {
-    // NavBar for Review
+    );
         //Stuff for View Change Requests, Logout
-    } else if (navBarNumber == 3) {
+    } else if (navBarNumber === 3) {
     // NavBar for ChangeRequestsPage
+        <div>
+            <div id="u173" class="ax_default button">
+                <div id="u173_div" class=""></div>
+                <a href="/Entry" id="u173_text" class="text ">
+                <p><span>Submit Change</span></p>
+                    </a>
+                </div>
+
+                <div id="u174" class="ax_default button">
+            <div id="u174_div" class=""></div>
+            <a href="/ArchivedChangesApp" id="u174_text" class="text ">
+              <p><span>View Change Request Archive</span></p>
+                </a>
+            </div>
+
+            <div id="u173" class="ax_default button">
+            <div id="u173_div" class=""></div>
+            <a href="/Login" id="u173_text" class="text ">
+              <p><span>Log out</span></p>
+                </a>
+            </div>
+        </div>
         //Stuff for Submit Request, View Change Requests, View Change Request Archive, Logout
     } else {
     // NavBar for ArchivedChangesPage
+        <div>
+            <div id="u173" class="ax_default button">
+                <div id="u173_div" class=""></div>
+                <a href="/Entry" id="u173_text" class="text ">
+                <p><span>Submit Change</span></p>
+                    </a>
+                </div>
+
+            <div id="u172" class="ax_default button">
+            <div id="u172_div" class=""></div>
+            <a href="/Review" id="u172_text" class="text">
+                <p><span>View Change Requests</span></p>
+            </a>
+            </div>
+
+            <div id="u173" class="ax_default button">
+            <div id="u173_div" class=""></div>
+            <a href="/Login" id="u173_text" class="text ">
+              <p><span>Log out</span></p>
+                </a>
+            </div>
+        </div>
         //Stuff for Submit Request, View Change Requests, View Change Request Archive, Logout
     }
     }
