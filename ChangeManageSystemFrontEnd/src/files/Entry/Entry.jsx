292005df2030808b37805126e135416e4ed3b589
chuckynuttys <108241843+chuckynuttys@users.noreply.github.com>
2023-11-03 15:22:52 -0500

    Fixing some css layout and html syntax
    The login is fixed I changed some incorrect syntax, login screen is fine, entry page is pretty solid. There is a <body> style that formats the background somewhere in the css files that needs to be fixed but I wont have time currently

--- ChangeManageSystemFrontEnd/src/files/Entry/Entry.jsx
+++ ChangeManageSystemFrontEnd/src/files/Entry/Entry.jsx
@@ -5,40 +5,42 @@ import './Entry.css';
 import backgroundImg from '../../Images/BackgroundImg.jpg';
 import logo from '../../Images/Logo.png';
 import NavBar from "../../Components/NavBar";
-
 var navBarNumber;
+
 const Entry = () => {
   return (
-    
-      <form> 
+    <div>
+    <form>
+    <NavBar navBarNumber={1}/>
           <div className="menu">
              <h1> <img className = "logo" src={logo} alt="logo" /> </h1>
              <h2>Change Request Form</h2>
-             <fieldset className="fs1">
+             <fieldset>
               <label className= "labelInput" htmlFor="aId">
-              <input className="inputLabelTopPage" id="aId" name = "aID" rows="9" cols="50" placeholder="Application ID" />
+              <input 
+              className="inputLabelTopPage" id="applicationID" name = "applicationID" rows="9" cols="50" placeholder="Application ID" />
               <p className = "button">Three character ID of the application team implementing the change</p>
               </label>
               <label className= "labelInput" htmlFor="descr">
-              <textarea rows="3" cols="30" placeholder="Description" id="descr"></textarea>
+              <textarea id="description" name = "description" rows="3" cols="30" placeholder="Description"></textarea>
               <p className = "button">80 Character description of what the change is</p>
               </label>
               <label className= "labelInput"  htmlFor="why">
-             <textarea rows="3" cols="30" placeholder="Why" id="why"></textarea>
+             <textarea rows="3" cols="30" placeholder="Why" name = "why" id="why"></textarea>
               <p className = "button">Why is the change needed</p>
               </label>
              </fieldset>
              <fieldset className="fs2">
              <label className= "labelInput" htmlFor="whatIs">
-                <input className="inputLabelTopPage" id="whatIs" name="whatIs" rows='9' cols = '50' placeholder="What is changing?" />
+                <input className="inputLabelTopPage" id="whatIsChanging" name="whatIsChanging" rows='9' cols = '50' placeholder="What is changing?" />
                 <p className = "button">New version of software, server configuration changes, etc..</p>
               </label>
               <label className= "labelInput" htmlFor="backOut">
-              <textarea rows="3" cols="30" placeholder="Back-Out Plan" id="backOut"></textarea>
+              <textarea rows="3" cols="30" placeholder="Back-Out Plan" id="backOutPlan" name = "backOutPlan"></textarea>
               <p className = "button">Description of what it takes to revert the change</p>
               </label>
               <label className= "labelInput" htmlFor="backOutMins">
-                <input className="inputLabelTopPage" id="backOutMins" name="backOutMins" rows='9' cols = '50' placeholder="Back-Out Minutes" />
+                <input className="inputLabelTopPage" id="backOutMinutes" name="backOutMinutes" rows='9' cols = '50' placeholder="Back-Out Minutes" />
                 <p className = "button">How long will it take to perform the back-out in minutes</p>
               </label>
              </fieldset>
@@ -104,7 +106,7 @@ const Entry = () => {
              </fieldset>
           </div>
           </form>
-      
+          </div>
   );
-};
+  };
 export default Entry;
--- ChangeManageSystemFrontEnd/src/files/Login/Login.jsx
+++ ChangeManageSystemFrontEnd/src/files/Login/Login.jsx
@@ -18,40 +18,40 @@ const Login = () => {
   
 
   return (
-    <body>
-    <div id="base" class="">
+    <div>
+    <div id="base" className="">
 
-      <div id="u0" class="ax_default image">
-        <img id="u0_img" class="img " src={backgroundImg}/>
-        <div id="u0_text" class="text ">
+      <div id="u0" className="ax_default image">
+        <img id="u0_img" className="img " src={backgroundImg}/>
+        <div id="u0_text" className="text ">
           <p></p>
         </div>
       </div>
 
-      <div id="u1" class="ax_default box_2">
-        <div id="u1_div" class=""></div>
-        <div id="u1_text" class="text ">
+      <div id="u1" className="ax_default box_2">
+        <div id="u1_div" className=""></div>
+        <div id="u1_text" className="text ">
           <p></p>
         </div>
       </div>
 
-      <div id="u2" class="ax_default box_3">
-        <div id="u2_div" class=""></div>
-        <div id="u2_text" class="text ">
+      <div id="u2" className="ax_default box_3">
+        <div id="u2_div" className=""></div>
+        <div id="u2_text" className="text ">
           <p></p>
         </div>
       </div>
 
-      <div id="u3" class="ax_default heading_1">
-        <div id="u3_div" class=""></div>
-        <div id="u3_text" class="text ">
+      <div id="u3" className="ax_default heading_1">
+        <div id="u3_div" className=""></div>
+        <div id="u3_text" className="text ">
           <p><span>Log In to Change Request System</span></p>
         </div>
       </div>
 
-      <div id="u4" class="ax_default image">
-        <img id="u4_img" class="img " src={logo}/>
-        <div id="u4_text" class="text ">
+      <div id="u4" className="ax_default image">
+        <img id="u4_img" className="img " src={logo}/>
+        <div id="u4_text" className="text ">
           <p></p>
         </div>
       </div>
@@ -59,53 +59,53 @@ const Login = () => {
       <LoginComponent handleSignIn={handleSignIn}/>
 
       {/* Upper Label UserID */}
-      <div id="u7" class="ax_default shape ax_default_hidden selected" data-label="Upper Label" style={{display: 'none', visibility: 'hidden'}}>
-        <div id="u7_div" class="selected"></div>
-        <div id="u7_text" class="text ">
+      <div id="u7" className="ax_default shape ax_default_hidden selected" data-label="Upper Label" style={{display: 'none', visibility: 'hidden'}}>
+        <div id="u7_div" className="selected"></div>
+        <div id="u7_text" className="text ">
           <p><span>UserID</span></p>
         </div>
       </div>
 
       {/* Upper Label Password */}
-      <div id="u11" class="ax_default shape ax_default_hidden selected" data-label="Upper Label" style={{display: 'none', visibility: 'hidden'}}>
-        <div id="u11_div" class="selected"></div>
-        <div id="u11_text" class="text ">
+      <div id="u11" className="ax_default shape ax_default_hidden selected" data-label="Upper Label" style={{display: 'none', visibility: 'hidden'}}>
+        <div id="u11_div" className="selected"></div>
+        <div id="u11_text" className="text ">
           <p><span>Password</span></p>
         </div>
       </div>
 
-      <div id="u14" class="ax_default paragraph ax_default_hidden" data-label="Password Required" style={{display: 'none', visibility: 'hidden'}}>
-        <div id="u14_div" class=""></div>
-        <div id="u14_text" class="text ">
+      <div id="u14" className="ax_default paragraph ax_default_hidden" data-label="Password Required" style={{display: 'none', visibility: 'hidden'}}>
+        <div id="u14_div" className=""></div>
+        <div id="u14_text" className="text ">
           <p><span>This field is required.</span></p>
         </div>
       </div>
 
-      <div id="u15" class="ax_default paragraph ax_default_hidden" data-label="Invalid Password" style={{display: 'none', visibility: 'hidden'}}>
-        <div id="u15_div" class=""></div>
-        <div id="u15_text" class="text ">
+      <div id="u15" className="ax_default paragraph ax_default_hidden" data-label="Invalid Password" style={{display: 'none', visibility: 'hidden'}}>
+        <div id="u15_div" className=""></div>
+        <div id="u15_text" className="text ">
           <p><span>Invalid Password</span></p>
         </div>
       </div>
 
-      <div id="u16" class="ax_default paragraph ax_default_hidden" data-label="UserID Required" style={{display: 'none', visibility: 'hidden'}}>
-        <div id="u16_div" class=""></div>
-        <div id="u16_text" class="text ">
+      <div id="u16" className="ax_default paragraph ax_default_hidden" data-label="UserID Required" style={{display: 'none', visibility: 'hidden'}}>
+        <div id="u16_div" className=""></div>
+        <div id="u16_text" className="text ">
           <p><span>This field is required.</span></p>
         </div>
       </div>
 
-      <div id="u17" class="ax_default paragraph ax_default_hidden" data-label="Invalid UserID" style={{display: 'none', visibility: 'hidden'}}>
-        <div id="u17_div" class=""></div>
-        <div id="u17_text" class="text ">
+      <div id="u17" className="ax_default paragraph ax_default_hidden" data-label="Invalid UserID" style={{display: 'none', visibility: 'hidden'}}>
+        <div id="u17_div" className=""></div>
+        <div id="u17_text" className="text ">
           <p><span>Invalid UserID.</span></p>
         </div>
       </div>
     </div>
     <script src="resources/scripts/axure/ios.js"></script>
-  </body>
+  </div>
   );
-};
+  };
 
 export default Login;
 
